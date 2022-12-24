5afdac3f911736a5371f318c166acb7f786e7a48
sofaki000 <58658186+sofaki000@users.noreply.github.com>
2022-12-24 19:50:22 +0200

    removed unused function

--- data_utilities/preprocessing.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import MinMaxScaler
-
-
-def scale_data(X, y):
-    scaler_X = MinMaxScaler()
-    scaler_Y = MinMaxScaler()
-    X = scaler_X.fit_transform(X)
-    return X, y
